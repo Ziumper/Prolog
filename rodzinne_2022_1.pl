a6d0344ce9f15991f0c35fd4e4322eaba2472f71
Tomasz Komoszeski <tomasz.komoszeski@gmail.com>
2022-04-03 15:19:56 +0200

    Update rodzinne_2022_1.pl

--- rodzinne_2022_1.pl
+++ rodzinne_2022_1.pl
@@ -1,17 +1,113 @@
-rodzice(jan,ewa,[adam],[maria]).
-rodzice(adam,halina,[norbert],[monika]).
-rodzice(piotr,maria,[krzysztof]).
-ojciec(edward,[],[halina]).
-matka(monika,[maciej],[]).
+rodzice(jan,ewa,adam,m).
+rodzice(jan,ewa,maria,k).
+rodzice(adam,halina,monika,k).
+rodzice(adam,halina,norbert,m).
+rodzice(piotr,maria,krzysztof,m).
+ojciec(edward,halina,k).
+matka(monika,maciej,m).
 
-% element(b,[a,b,c]). %odpowiedz true
-% element(X,[a,b,c]). %wszystkie wartosci spelniajace zapytanie x=a, x=b
-% , x=c
+%podkreslenie _ oznacza dowolna literke
+dziecko(ImieDziecka,ImieRodzica):-
+    rodzice(ImieRodzica,_,ImieDziecka,_).
+dziecko(ImieDziecka,ImieRodzica):-
+    rodzice(_,ImieRodzica,ImieDziecka,_).
+dziecko(ImieDziecka,ImieRodzica):-
+    ojciec(ImieRodzica,ImieDziecka,_).
+dziecko(ImieDziecka,ImieRodzica):-
+    matka(ImieRodzica,ImieDziecka,_).
 
-%element(E,[G|O]):- E == G. rownoazne jest temu co ponizej
-element(E,[E|_]). %_ podkreslnik dowolna literka
-element(E,[_|O]):- element(E,O).
+syn(ImieDziecka,ImieOjca,ImieMatki):-
+    rodzice(ImieOjca,ImieMatki,ImieDziecka,m).
+syn(ImieDziecka,ImieOjca):-
+    ojciec(ImieOjca,ImieDziecka,m).
+syn(ImieDziecka,ImieMatki):-
+    matka(ImieMatki,ImieDziecka,m).
 
+córka(ImieDziecka,ImieOjca,ImieMatki):-
+    rodzice(ImieOjca,ImieMatki,ImieDziecka,k).
+córka(ImieDziecka,ImieOjca):-
+    ojciec(ImieOjca,ImieDziecka,k).
+córka(ImieDziecka,ImieMatki):-
+    matka(ImieMatki,ImieDziecka,k).
+
+zona(ImieZony,ImieMeza):-
+    rodzice(ImieMeza,ImieZony,_,_).
+
+maz(ImieMeza,ImieZony):-
+    rodzice(ImieMeza,ImieZony,_,_).
+
+dziadek(ImieDziadka,ImieWnuczenta):-
+    ojciec(ImieDziadka,ImieRodzica,_),
+    rodzice(_,ImieRodzica,ImieWnuczenta,_).
+dziadek(ImieDziadka,ImieWnuczenta):-
+    ojciec(ImieDziadka,ImieRodzica,_),
+    rodzice(ImieRodzica,_,ImieWnuczenta,_).
+dziadek(ImieDziadka,ImieWnuczenta):-
+    rodzice(ImieDziadka,_,ImieRodzica,_),
+    dziecko(ImieWnuczenta,ImieRodzica).
+
+babcia(ImieBabci,ImieWnuczenta):-
+    matka(ImieBabci,ImieRodzica,_),
+    rodzice(_,ImieRodzica,ImieWnuczenta,_).
+babcia(ImieBabci,ImieWnuczenta):-
+    matka(ImieBabci,ImieRodzica,_),
+    rodzice(ImieRodzica,_,ImieWnuczenta,_).
+babcia(ImieBabci,ImieWnuczenta):-
+    rodzice(_,ImieBabci,ImieRodzica,_),
+    dziecko(ImieWnuczenta,ImieRodzica).
+
+%brat i siostra maja takich samych rodzicow
+brat(ImieBrata,Imie):-
+    rodzice(ImieOjca,ImieMatki,ImieBrata,m),
+    rodzice(ImieOjca,ImieMatki,Imie,_).
+
+siostra(ImieSiostry,Imie):-
+    rodzice(ImieOjca,ImieMatki,ImieSiostry,k),
+    rodzice(ImieOjca,ImieMatki,Imie,_).
+
+stryj(ImieStryja,ImieSiostrzenca):-
+    rodzice(ImieOjca,_,ImieSiostrzenca,_),
+    brat(ImieStryja,ImieOjca).
+stryj(ImieStryja,ImieSiostrzenca):-
+    ojciec(ImieOjca,ImieSiostrzenca,_),
+    brat(ImieStryja,ImieOjca).
+
+wuj(ImieWuja,ImieSiostrzenca):-
+    rodzice(_,ImieMatki,ImieSiostrzenca,_),
+    brat(ImieWuja,ImieMatki).
+wuj(ImieWuja,ImieSiostrzenca):-
+    matka(ImieMatki,ImieSiostrzenca,_),
+    brat(ImieWuja,ImieMatki).
+
+tesciowa(ImieTesciowej,ImiePoszkodowanego):-
+    maz(ImiePoszkodowanego,ImieZony),
+    dziecko(ImieZony,ImieTesciowej).
+
+
+bratanek(ImieBratanka,Imie):-
+    brat(ImieOjca,Imie),
+    ojciec(ImieOjca,ImieBratanka,m).
+bratanek(ImieBratanka,Imie):-
+    brat(ImieOjca,Imie),
+    rodzice(ImieOjca,_,ImieBratanka,m).
+
+potomek(ImiePotomka,ImiePrzodka):-
+    dziecko(ImiePotomka,ImiePrzodka).
+
+potomek(ImiePotomka,ImiePrzodka):-
+    dziecko(ImiePotomka,ImieRodzica),
+    potomek(ImieRodzica,ImiePrzodka).
+
+przodek(ImiePrzodka,ImiePotomka):-
+    potomek(ImiePotomka,ImiePrzodka).
+
+kuzyn(ImieKuzyna1,ImieKuzyna2):-
+    dziadek(ImieDziadka,ImieKuzyna1),
+    dziadek(ImieDziadka,ImieKuzyna2).
+
+krewni(ImieKrewnego1,ImieKrewnego2):-
+    przodek(ImiePrzodka,ImieKrewnego1),
+    przodek(ImiePrzodka,ImieKrewnego2).
 
 %zliczajacy meta predykat
 %P - formula zwracajaca wyniki
@@ -24,170 +120,37 @@ count(P,Count) :-
 liczba_przodkow(Licznik,ImiePotomka):-
     count(przodek(_,ImiePotomka),Licznik).
 
+matka_troj(Odp,ImieMatki,ImieDziecka) :-
+    rodzice(_,ImieMatki,ImieDziecka,_),
+    string_chars(Odp,[t,a,k]).
 
-kuzyn(ImieKuzyna1,ImieKuzyna2):-
-    dziadek(ImieDziadka,ImieKuzyna1),
-    dziadek(ImieDziadka,ImieKuzyna2),
-    ImieKuzyna1 \== ImieKuzyna2,
-    \+brat(ImieKuzyna1,ImieKuzyna2),
-    \+siostra(ImieKuzyna1,ImieKuzyna2),
-    \+brat(ImieKuzyna2,ImieKuzyna1),
-    \+siostra(ImieKuzyna2,ImieKuzyna1).
+matka_troj(Odp,ImieMatki,ImieDziecka) :-
+    matka(ImieMatki,ImieDziecka,_),
+    string_chars(Odp,[t,a,k]).
 
-krewni(ImieKrewnego1,ImieKrewnego2):-
-    przodek(ImiePrzodka,ImieKrewnego1),
-    przodek(ImiePrzodka,ImieKrewnego2),
-    ImieKrewnego1 \== ImieKrewnego2.
+matka_troj(Odp,ImieMatki,ImiePrzodka) :-
+    przodek(ImiePrzodka,ImieMatki),
+    string_chars(Odp,[n,i,e]).
 
-
-przodek(ImiePrzodka,ImiePotomka):-
-    potomek(ImiePotomka,ImiePrzodka).
-
-potomek(ImiePotomka,ImiePrzodka):-
-    dziecko(ImiePotomka,ImiePrzodka).
-
-potomek(ImiePotomka,ImiePrzodka):-
-    dziecko(ImiePotomka,ImieRodzica),
-    potomek(ImieRodzica,ImiePrzodka).
-
-bratanek(ImieBratanka,ImieStryja):-
-    stryj(ImieStryja,ImieBratanka).
+matka_troj(Odp,_,ImieDziecka) :-
+     dziecko(ImieDziecka,_) ->
+        string_chars(Odp,[n,i,e]) ; string_chars(Odp,[b,r,a,k,' ',d,a,n,y,c,h]).
 
 
-tesciowa(ImieTesciowej,ImieMeza):-
-    maz(ImieMeza,Zona),
-    plec(ImieTesciowej,k),
-    rodzic(ImieTesciowej,Zona).
 
-maz(ImieMeza,ImieZony):-
-    zona(ImieZony,ImieMeza).
-
-zona(ImieZony,ImieMeza):-
-    rodzice(ImieMeza,ImieZony,_,_).
-zona(ImieZony,ImieMeza):-
-    rodzice(ImieMeza,ImieZony,_).
-
-wuj(Wuj,Siostrzeniec):-
-    plec(Wuj,m),
-    rodzic(Rodzic,Siostrzeniec),
-    plec(Rodzic,k),
-    brat(Wuj,Rodzic).
-
-stryj(Stryj,Siostrzeniec):-
-    plec(Stryj,m),
-    rodzic(Rodzic,Siostrzeniec),
-    plec(Rodzic,m),
-    brat(Rodzic,Stryj).
-
-dziadek(Dziadek,Wnuk):-
-    rodzic(Dziadek,Rodzic),
-    rodzic(Rodzic,Wnuk),
-    plec(Dziadek,m).
-
-babcia(Babcia,Wnuk):-
-    rodzic(Babcia,Rodzic),
-    rodzic(Rodzic,Wnuk),
-    plec(Babcia,k).
-
-syn(Syn,Rodzic):-
-    rodzic(Rodzic,Syn),
-    plec(Syn,m).
-
-corka(Corka,Rodzic):-
-    rodzic(Rodzic,Corka),
-    plec(Corka,k).
+siostra_troj(Odp,ImieSiostry,ImieCzlonkaRodziny):-
+    siostra(ImieSiostry,ImieCzlonkaRodziny) ->
+       string_chars(Odp,[t,a,k]) ;
+          count(siostra(_,ImieCzlonkaRodziny),LicznikSiostry), %czy ten gość ma jakąkolwiek siostrę?
+          LicznikSiostry > 0 -> string_chars(Odp,[n,i,e]) ; string_chars(Odp,[b,r,a,k,' ',d,a,n,y,c,h]).
 
 
-%dziecko
-dziecko(Dziecko,Rodzic) :- rodzic(Rodzic,Dziecko),
-    Dziecko \== Rodzic.
 
 
-brat(ImieBrata,Y):-
-    rodzic(R,ImieBrata),
-    rodzic(R,Y),
-    plec(ImieBrata,m),
-    ImieBrata \== Y.
-
-siostra(ImieSiostra,Y):-
-    rodzic(R,ImieSiostra),
-    rodzic(R,Y),
-    plec(ImieSiostra,k),
-    ImieSiostra \== Y.
-
-plec(X,m) :-
-    rodzice(_,_,Lista_s),
-    element(X,Lista_s).
-plec(X,m) :-
-    rodzice(_,_,Lista_s,_),
-    element(X,Lista_s).
-plec(X,m):-
-    ojciec(_,Lista_s,_),
-    element(X,Lista_s).
-plec(X,m):-
-    matka(_,Lista_s,_),
-    element(X,Lista_s).
-plec(X,m):-
-    ojciec(X,_,_).
-plec(X,m):-
-    rodzice(X,_,_,_).
-
-plec(X,k):-
-    rodzice(_,_,_,Lista_s),
-    element(X,Lista_s).
-plec(X,k):-
-    rodzice(_,X,_,_).
-plec(X,k):-
-    ojciec(_,_,Lista_c),
-    element(X,Lista_c).
-plec(X,k):-
-    matka(_,_,Lista_c),
-    element(X,Lista_c).
-plec(X,k):-
-    matka(X,_,_).
 
 
-%synowie
-rodzic(X,Y):- ojciec(X,Lista_s,_),
-    element(Y,Lista_s),
-    X \== Y.
-rodzic(X,Y):- matka(X,Lista_s,_),
-    element(Y,Lista_s),
-    X \== Y.
-%ojciec/syn
-rodzic(X,Y):- rodzice(X,_,Lista_s,_),
-    element(Y,Lista_s),
-    X \==Y.
-rodzic(X,Y):- rodzice(X,_,Lista_s),
-    element(Y,Lista_s),
-    X \==Y.
-
-%matka/syn
-rodzic(X,Y):- rodzice(_,X,Lista_s),
-    element(Y,Lista_s),
-    X \== Y.
-rodzic(X,Y):- rodzice(_,X,Lista_s,_),
-    element(Y,Lista_s),
-    X \== Y.
-
-%matka corka
-rodzic(X,Y):- rodzice(_,X,_,Lista_c),
-    element(Y,Lista_c),
-    X \== Y.
 
 
-rodzic(X,Y):- rodzice(X,_,_,Lista_c),
-    element(Y,Lista_c),
-    X \== Y.
-
-%ojciec corka
-rodzic(X,Y):- ojciec(X,_,Lista_c),
-    element(Y,Lista_c),
-    X \== Y.
-
-rodzic(X,Y):- matka(X,_,Lista_c),
-    element(Y,Lista_c),
-    X \== Y.
 
 
 
